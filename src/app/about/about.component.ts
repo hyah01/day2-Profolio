507d0e15b3a055c3e83519e5ee8d8514371a449e
Hy <hyhuynh222@gmail.com>
2024-06-25 14:47:22 -0700

    Added About page
    With Icons

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  isCollapsed = true;
+
+  toggleAccordion() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 
 }
